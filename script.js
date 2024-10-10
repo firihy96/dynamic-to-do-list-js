04bdb55639cc3197b22f58fa17bf90f632cd3763
Abdullah Adel Firihy <144290641+firihy96@users.noreply.github.com>
2024-10-10 11:51:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded',()=>{
   let addButton = document.getElementById('add-task-btn'),
-    taskInput =  document.getElementById('task-list'),
-    taskList =  document.getElementById('task-input');
+    taskInput =  document.getElementById('task-input'),
+    taskList =  document.getElementById('task-list');
   function addTask() => {
     let taskText = taskInput.value.trim();
     if (!taskText) {
       alert('Please, Enter a task to be added');
     } else {
-      let taskElement =  document.creatElement('li');
+      let taskElement =  document.creatElement('li'),
+        removeBtn = document.createElement('button');
       taskElement.textConent = taskText;
       taskList.appendChild(taskElement);
       taskInput.value = '';
+      removeBtn.textContent = 'Remove';
+      removeBtn.classList.add('remove-btn');
+      removeBtn.onClick = function (e){
+        taskList.removeChild(e.target.parentNode);
+      }
+      taskElement.appendChild('removeBtn');
     }
 
     addButton.addEventListener('keypress',(event)=>{
