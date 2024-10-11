f23a363e3f877146fb5f4ff4f068168066cd788f
Abdullah Firihy <abdullahfirihy96@outlook.com>
2024-10-11 17:12:21 +0300

    fixing removeBtn issue

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  let addButton = document.getElementById('add-task-btn'),
-    taskInput =  document.getElementById('task-input'),
-    taskList =  document.getElementById('task-list');
-  function addTask() => {
+document.addEventListener("DOMContentLoaded", () => {
+  let addButton = document.getElementById("add-task-btn"),
+    taskInput = document.getElementById("task-input"),
+    taskList = document.getElementById("task-list");
+  function addTask() {
     let taskText = taskInput.value.trim();
     if (!taskText) {
-      alert('Please, Enter a task to be added');
+      alert("Please, Enter a task to be added");
     } else {
-      let taskElement =  document.creatElement('li'),
-        removeBtn = document.createElement('button');
-      taskElement.textConent = taskText;
+      let taskElement = document.createElement("li"),
+        removeBtn = document.createElement("button");
+      taskElement.textContent = taskText;
       taskList.appendChild(taskElement);
-      taskInput.value = '';
-      removeBtn.textContent = 'Remove';
-      removeBtn.classList.add('remove-btn');
-      removeBtn.onClick = function (e){
-        taskList.removeChild(e.target.parentNode);
-      }
-      taskElement.appendChild('removeBtn');
+      taskInput.value = "";
+      removeBtn.textContent = "Remove";
+      taskElement.appendChild(removeBtn);
+      removeBtn.classList.add("remove-btn");
+      removeBtn.addEventListener('click',(e)=>{
+        taskList.removeChild(e.target.parentNode)
+      })
     }
-
-   taskInput.addEventListener('keypress',(event)=>{
-      if (event.key === 'Enter') {
-        addTask();
-      }
-    })
-  addButton.addEventListener('click', ()=>{
-     addTask();
-  })
-    
   }
-  
-})
+  taskInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+  addButton.addEventListener("click", () => {
+    addTask();
+  });
+});
